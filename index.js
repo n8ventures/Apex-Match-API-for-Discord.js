7748d6f8a30145d45344f6c59d023e2348966617
unknown <n8ventures@gmail.com>
2021-03-07 22:06:50 +0800

    12 commit: fixed dates. Added Ping stats.

--- index.js
+++ index.js
@@ -421,8 +421,8 @@ message.channel.send({ embed });
 
 			case '.ping':
 			 setTimeout(function(){message.channel.send(`🏓 *hehe*.`)} ,1000); 
-			 setTimeout(function(){message.channel.send(`\n **Latency** is __**${Date.now() - message.createdTimestamp - 2000}ms!**__`)} ,2000); 
-			 setTimeout(function(){message.channel.send(` \n **Discord API Latency** is __**${Math.round(client.ws.ping)}ms!**__`)} ,3000); 
+			 setTimeout(function(){message.channel.send(`\n 📡 **Latency** is __**${Date.now() - message.createdTimestamp - 2000}ms!**__`)} ,2000); 
+			 setTimeout(function(){message.channel.send(` \n 🛰 **Discord API Latency** is __**${Math.round(client.ws.ping)}ms!**__`)} ,3000); 
 			break;
 	}
 });
